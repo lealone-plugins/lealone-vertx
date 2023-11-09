da1b7bbd8ac7a931654b6dbdfec5ba1f6cb06ca1
Honghua Zhu <zhh200910@gmail.com>
2023-11-09 08:32:27 +0800

    增加web测试例子

--- src/main/java/org/lealone/plugins/vertx/VertxServerEngine.java
+++ src/main/java/org/lealone/plugins/vertx/VertxServerEngine.java
@@ -5,35 +5,19 @@
  */
 package org.lealone.plugins.vertx;
 
-import java.util.Map;
-
 import org.lealone.plugins.service.http.HttpServerEngine;
 import org.lealone.server.ProtocolServer;
 
 public class VertxServerEngine extends HttpServerEngine {
 
-    public static final String NAME = "vertx";
-    private VertxServer server; // 延迟初始化
+    public static final String NAME = "Vertx";
 
     public VertxServerEngine() {
         super(NAME);
     }
 
     @Override
-    public ProtocolServer getProtocolServer() {
-        if (server == null)
-            server = new VertxServer();
-        return server;
-    }
-
-    @Override
-    public void init(Map<String, String> config) {
-        super.init(config);
-        getProtocolServer().init(config);
-    }
-
-    @Override
-    public void close() {
-        getProtocolServer().stop();
+    protected ProtocolServer createProtocolServer() {
+        return new VertxServer();
     }
 }
